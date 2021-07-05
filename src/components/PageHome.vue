89ac004472cd12778326f9a7de3ad6fd9b4ece80
Hesham <heshamelmasry1992@gmail.com>
2021-07-05 14:24:08 +0200

    add new eslint rule, add data file, new component for home page, iterate on data

---
+++ src/components/PageHome.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{userById(postById(postId).userId).name}}</p>
+      <p>{{postById(postId).text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json';
+
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    }
+  },
+  methods:{
+    postById(postId){
+      return this.posts.find(p => p.id === postId)
+    },
+    userById(userId){
+      return this.users.find(u => u.id === userId)
+    },
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
